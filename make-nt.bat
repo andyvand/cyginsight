5213624e74543354bb1011ec1ec2d49521bcc125
wylie <wylie>
1998-04-24 18:10:02 +0000

    Added check and auto-create of installation directories

--- make-nt.bat
+++ make-nt.bat
@@ -1,7 +1,15 @@
 rem Script for building Paradyn & DynInstAPI on WinNT platform
 @echo off
-rem $Id: make-nt.bat,v 1.4 1998/04/03 22:45:26 wylie Exp $
+rem $Id: make-nt.bat,v 1.5 1998/04/24 18:10:02 wylie Exp $
 @echo on
+
+set LIBRARY_DEST="..\lib"
+set PROGRAM_DEST="..\bin"
+
+if not exist %LIBRARY_DEST% mkdir %LIBRARY_DEST%
+if not exist %LIBRARY_DEST%\%PLATFORM% mkdir %LIBRARY_DEST%\%PLATFORM%
+if not exist %PROGRAM_DEST% mkdir %PROGRAM_DEST%
+if not exist %PROGRAM_DEST%\%PLATFORM% mkdir %PROGRAM_DEST%\%PLATFORM%
 
 cmd /c "cd util\%PLATFORM% && nmake clean && nmake install"
 cmd /c "cd igen\%PLATFORM% && nmake clean && nmake install"
@@ -10,4 +18,3 @@ cmd /c "cd paradynd\%PLATFORM% && nmake clean && nmake install"
 cmd /c "cd dyninstAPI\%PLATFORM% && nmake clean && nmake install"
 cmd /c "cd dyninstAPI_RT\%PLATFORM% && nmake clean && nmake install"
 cmd /c "cd dyninstAPI\tests\%PLATFORM% && nmake clean && nmake install"
-
