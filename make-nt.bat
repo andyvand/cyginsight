ba475aaec32b001c9c18c52f0dc3d0ae7a5ddd3e
schendel <schendel>
2002-07-19 21:58:16 +0000

    fixed a bug in make-nt.bat which caused compilation to fail on core directories which were clean (ie. no build files to remove)

--- make-nt.bat
+++ make-nt.bat
@@ -1,6 +1,6 @@
 rem Script for building Paradyn & DynInstAPI on WinNT platform
 @echo off
-rem $Id: make-nt.bat,v 1.12 2002/04/17 17:07:56 schendel Exp $
+rem $Id: make-nt.bat,v 1.13 2002/07/19 21:58:16 schendel Exp $
 @echo on
 
 set LIBRARY_DEST="..\lib"
@@ -11,22 +11,37 @@ if not exist %LIBRARY_DEST%\%PLATFORM% mkdir %LIBRARY_DEST%\%PLATFORM%
 if not exist %PROGRAM_DEST% mkdir %PROGRAM_DEST%
 if not exist %PROGRAM_DEST%\%PLATFORM% mkdir %PROGRAM_DEST%\%PLATFORM%
 
-cmd /c "cd pdutil\%PLATFORM% && nmake clean && nmake install"
-cmd /c "cd igen\%PLATFORM% && nmake clean && nmake install"
-cmd /c "cd rtinst\%PLATFORM% && nmake clean && nmake install"
-cmd /c "cd paradynd\%PLATFORM% && nmake clean && nmake install"
-cmd /c "cd thread\%PLATFORM% && nmake clean && nmake install"
-cmd /c "cd paradyn\%PLATFORM% && nmake clean && nmake install"
-cmd /c "cd visi\%PLATFORM% && nmake clean && nmake install"
-cmd /c "cd visiClients\histVisi\%PLATFORM% && nmake clean && nmake install"
-cmd /c "cd visiClients\barchart\%PLATFORM% && nmake clean && nmake install"
-cmd /c "cd visiClients\tableVisi\%PLATFORM% && nmake clean && nmake install"
-cmd /c "cd visiClients\phaseTable\%PLATFORM% && nmake clean && nmake install"
-cmd /c "cd visiClients\tclVisi\%PLATFORM% && nmake clean && nmake install"
+cmd /c "cd pdutil\%PLATFORM% && nmake clean"
+cmd /c "cd pdutil\%PLATFORM% && nmake install"
+cmd /c "cd igen\%PLATFORM% && nmake clean"
+cmd /c "cd igen\%PLATFORM% && nmake install"
+cmd /c "cd rtinst\%PLATFORM% && nmake clean"
+cmd /c "cd rtinst\%PLATFORM% && nmake install"
+cmd /c "cd paradynd\%PLATFORM% && nmake clean"
+cmd /c "cd paradynd\%PLATFORM% && nmake install"
+cmd /c "cd thread\%PLATFORM% && nmake clean"
+cmd /c "cd thread\%PLATFORM% && nmake install"
+cmd /c "cd paradyn\%PLATFORM% && nmake clean"
+cmd /c "cd paradyn\%PLATFORM% && nmake install"
+cmd /c "cd visi\%PLATFORM% && nmake clean"
+cmd /c "cd visi\%PLATFORM% && nmake install"
+cmd /c "cd visiClients\histVisi\%PLATFORM% && nmake clean"
+cmd /c "cd visiClients\histVisi\%PLATFORM% && nmake install"
+cmd /c "cd visiClients\barchart\%PLATFORM% && nmake clean"
+cmd /c "cd visiClients\barchart\%PLATFORM% && nmake install"
+cmd /c "cd visiClients\tableVisi\%PLATFORM% && nmake clean"
+cmd /c "cd visiClients\tableVisi\%PLATFORM% && nmake install"
+cmd /c "cd visiClients\phaseTable\%PLATFORM% && nmake clean"
+cmd /c "cd visiClients\phaseTable\%PLATFORM% && nmake install"
+cmd /c "cd visiClients\tclVisi\%PLATFORM% && nmake clean"
+cmd /c "cd visiClients\tclVisi\%PLATFORM% && nmake install"
 rem No visiClients/terrain to build for WinNT!
 
-cmd /c "cd dyninstAPI_RT\%PLATFORM% && nmake clean && nmake install"
-cmd /c "cd dyninstAPI\%PLATFORM% && nmake clean && nmake install"
-cmd /c "cd dyninstAPI\tests\%PLATFORM% && nmake clean && nmake install"
-cmd /c "cd dyner\%PLATFORM% && nmake clean && nmake install"
-
+cmd /c "cd dyninstAPI_RT\%PLATFORM% && nmake clean"
+cmd /c "cd dyninstAPI_RT\%PLATFORM% && nmake install"
+cmd /c "cd dyninstAPI\%PLATFORM% && nmake clean"
+cmd /c "cd dyninstAPI\%PLATFORM% && nmake install"
+cmd /c "cd dyninstAPI\tests\%PLATFORM% && nmake clean"
+cmd /c "cd dyninstAPI\tests\%PLATFORM% && nmake install"
+cmd /c "cd dyner\%PLATFORM% && nmake clean"
+cmd /c "cd dyner\%PLATFORM% && nmake install"
